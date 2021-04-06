b8986b25660f9695dab869411daa21372f40ceca
Oliver Mascarenhas <oliver.savio@gmail.com>
2021-04-06 16:33:49 +0530

    code for softmax activation fucntion

---
+++ softmax_classification/go.mod
@@ -0,0 +1,13 @@
+module github.com/oliversavio/learn-ml-with-go/softmax_classification
+
+go 1.14
+
+require (
+	github.com/ajstarks/svgo v0.0.0-20200725142600-7a3c8b57fecb // indirect
+	golang.org/x/exp v0.0.0-20210220032938-85be41e4509f // indirect
+	golang.org/x/image v0.0.0-20210220032944-ac19c3e999fb // indirect
+	golang.org/x/text v0.3.6 // indirect
+	golang.org/x/tools v0.1.0 // indirect
+	gonum.org/v1/gonum v0.9.1
+	gonum.org/v1/netlib v0.0.0-20210302091547-ede94419cf37 // indirect
+)
---
+++ softmax_classification/main.go
@@ -0,0 +1,59 @@
+package main
+
+import "fmt"
+import "gonum.org/v1/gonum/mat"
+import "math"
+import "math/rand"
+
+func main() {
+	rand.Seed(350)
+	nn := linear()
+	fmt.Println("Activation after linear layer")
+	fmt.Println(mat.Formatted(nn))
+
+	nnsmax := softmax(nn)
+	fmt.Println("Activation after softmax layer")
+	fmt.Println(mat.Formatted(nnsmax))
+
+	var ressum float64
+	for _, v := range nnsmax.RawMatrix().Data {
+		ressum += v
+	}
+
+	fmt.Println(ressum)
+}
+
+func initRandomMatrix(row int, col int) *mat.Dense {
+	size := row * col
+	arr := make([]float64, size)
+	for i := range arr {
+		arr[i] = rand.NormFloat64()
+	}
+
+	return mat.NewDense(row, col, arr)
+}
+
+func softmax(matrix *mat.Dense) *mat.Dense {
+	var sum float64
+	for _, v := range matrix.RawMatrix().Data {
+		sum += math.Exp(v)
+	}
+
+	res := mat.NewDense(matrix.RawMatrix().Rows, matrix.RawMatrix().Cols, nil)
+	res.Apply(func(i int, j int, v float64) float64 {
+		return math.Exp(v) / sum
+	}, matrix)
+
+	return res
+}
+
+func linear() *mat.Dense {
+	m := initRandomMatrix(3, 3)
+	x := initRandomMatrix(3, 1)
+	c := initRandomMatrix(3, 1)
+
+	ll := mat.NewDense(3, 1, nil)
+	ll.Mul(m, x)
+	ll.Add(ll, c)
+	return ll
+}
